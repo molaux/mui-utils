9a02d567451640e06782081a0bf182fa7a1d37f4
Marc-Olivier Laux <marco@matlaux.net>
2022-02-24 13:23:33 +0100

    Add closable Status box

--- src/StatusTypography.jsx
+++ src/StatusTypography.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 
 import Alert from '@mui/material/Alert'
 import AlertTitle from '@mui/material/AlertTitle'
+import IconButton from '@mui/material/IconButton'
+import Collapse from '@mui/material/Collapse'
+import CloseIcon from '@mui/icons-material/Close'
 import { makeStyles } from '@mui/styles'
 
 const useStyles = makeStyles((theme) => ({
@@ -23,19 +26,46 @@ export const StatusTypography = ({
   children,
   gutterBottom,
   gutterTop,
+  persistent,
+  timeout,
   ...props
 }) => {
   const localClasses = useStyles()
+  const [open, setOpen] = React.useState(true)
+  useEffect(() => {
+    setOpen(true)
+    if (timeout !== null) {
+      const id = setTimeout(() => setOpen(false), timeout)
+      return () => clearTimeout(id)
+    }
+  }, [setOpen, timeout, children, title, error, warning, success, info])
+
   const classNames = `${gutterTop ? localClasses.gutterTop : ''} ${gutterBottom ? localClasses.gutterBottom : ''}`
   return (
-    <Alert
-      severity={error ? 'error' : warning ? 'warning' : success ? 'success' : 'info'}
-      {...props}
-      className={classNames}
-    >
-      {title ? <AlertTitle>{title}</AlertTitle> : null}
-      {children}
-    </Alert>
+    <Collapse in={open}>
+      <Alert
+        severity={error ? 'error' : warning ? 'warning' : success ? 'success' : 'info'}
+        {...props}
+        className={classNames}
+        action={
+          !persistent
+            ? <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={() => {
+                setOpen(false)
+              }}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+            : null
+        }
+      >
+        {title ? <AlertTitle>{title}</AlertTitle> : null}
+        {children}
+      </Alert>
+    </Collapse>
   )
 }
 
@@ -44,13 +74,15 @@ StatusTypography.propTypes = {
   warning: PropTypes.bool,
   success: PropTypes.bool,
   info: PropTypes.bool,
+  persistent: PropTypes.bool,
   title: PropTypes.string,
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node
   ]),
   gutterBottom: PropTypes.bool,
-  gutterTop: PropTypes.bool
+  gutterTop: PropTypes.bool,
+  timeout: PropTypes.number
 }
 
 StatusTypography.defaultProps = {
@@ -61,7 +93,9 @@ StatusTypography.defaultProps = {
   title: null,
   children: null,
   gutterBottom: false,
-  gutterTop: false
+  gutterTop: false,
+  persistent: true,
+  timeout: null
 }
 
 export default StatusTypography
